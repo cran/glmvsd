546a96b01e6dc75dd82f80642942de2ce9e78164
Yi Yang <yi.yang6@mcgill.ca>
2016-01-07 13:55:37 +0000

    version 1.4

--- R/glmvsd.R
+++ R/glmvsd.R
@@ -1,7 +1,9 @@
-glmvsd <- function(x, y, n_train = ceiling(n/2), no_rep = 100, model_check, 
-    psi = 1, family = c("gaussian", "binomial"), method = c("union", 
-        "customize"), candidate_models, weight_type = c("BIC", "AIC", "ARM"), 
-    prior = TRUE) {
+glmvsd <- function(x, y, n_train = ceiling(n/2), no_rep = 100, 
+					n_train_bound = n_train-2, n_bound = n-2,
+					model_check, psi = 1, 
+					family = c("gaussian", "binomial"), method = c("union", "customize"),
+                   	candidate_models, weight_type = c("BIC", "AIC", "ARM"), prior = TRUE,
+					reduce_bias = FALSE) {
     # check data and parameter
     family <- match.arg(family)
     method <- match.arg(method)
@@ -12,85 +14,101 @@ glmvsd <- function(x, y, n_train = ceiling(n/2), no_rep = 100, model_check,
     p <- NCOL(x)
     n <- length(y)
     if (family == "binomial") {
-        if (!all(y %in% c(0, 1))) 
-            stop("There can only be 0 or 1 in y when using binomial family")
+      if (!all(y %in% c(0, 1))) 
+        stop("There can only be 0 or 1 in y when using binomial family")
     }
-    # if (family == "tweedie") {
-    #     if (any(y < 0)) 
-    #         stop("y must be nonzero when using Tweedie family")
-    # }
     if (n != NROW(x)) 
         stop("x and y have different number of observations")
     if (n_train >= n) 
         stop("Training size must be less than the number of observations")
     if (missing(model_check)) 
         stop("User must provide a base model.")
+	if(is.vector(model_check)) model_check <- matrix(model_check,nrow=1)
     # use union option to compute candidate models
     if (method == "union") {
-        if (family == "gaussian") 
-            candidate_models <- gaussianfit(x, y)
-        if (family == "binomial") 
-            candidate_models <- binomialfit(x, y)
-        # if (family == "tweedie") 
-        #     candidate_models <- tweediefit(x, y)
+      if (family == "gaussian") 
+        candidate_models <- gaussianfit(x, y)
+      if (family == "binomial") 
+        candidate_models <- binomialfit(x, y)
     }
     if (method == "customize") {
-        if (missing(candidate_models)) 
-            stop("Users must supply a candidate model.")
-        if (is.matrix(candidate_models) != TRUE) 
-            stop("Supplied model must be a matrix.")
-        if (NCOL(candidate_models) != NCOL(x)) 
-            stop("Number of variables in candidate model and x does not match.")
-        if (!all(as.numeric(candidate_models) %in% c(0, 1))) 
-            stop("There can only be 0 or 1 in candidate_models")
+      if (missing(candidate_models)) 
+        stop("Users must supply a candidate model.")
+      if (!is.matrix(candidate_models)) 
+        stop("Supplied model must be a matrix.")
+      if (NCOL(candidate_models) != NCOL(x)) 
+        stop("Number of variables in candidate model and x does not match.")
+      if (!all(as.numeric(candidate_models) %in% c(0, 1))) 
+        stop("There can only be 0 or 1 in candidate_models")
     }
-    # clean the candidate models
+    # clean the candidate models mk
     candidate_models <- unique(candidate_models)
     rownames(candidate_models) <- NULL
-    candidate_models <- candidate_models[order(rowSums(candidate_models)), 
-        ]
-    candidate_models <- candidate_models[rowSums(candidate_models) < 
-        (n_train - 2), ]
+    candidate_models <- candidate_models[order(rowSums(candidate_models)), ]
+    if (weight_type == "ARM") {
+      candidate_models <- candidate_models[rowSums(candidate_models) < n_train_bound, ]
+    }else{
+	  candidate_models <- candidate_models[rowSums(candidate_models) < n_bound, ]
+    }
     # compute weights
     if (family == "gaussian") {
-        if (weight_type == "ARM") {
-            fit <- lsARM(x = x, y = y, candidate_models = candidate_models, 
-                n_train = n_train, no_rep = no_rep, psi = psi, prior = prior)
-        }
-        if (weight_type == "AIC") {
-            fit <- lsIC(x = x, y = y, candidate_models = candidate_models, 
-                psi = psi, prior = prior, type = "AIC")
-        }
-        if (weight_type == "BIC") {
-            fit <- lsIC(x = x, y = y, candidate_models = candidate_models, 
-                psi = psi, prior = prior, type = "BIC")
-        }
+      if (weight_type == "ARM") {
+        fit <- lsARM(x = x, y = y, candidate_models = candidate_models, 
+                     n_train = n_train, no_rep = no_rep, psi = psi, prior = prior)
+      }
+      if (weight_type == "AIC" | weight_type == "BIC") {
+        fit <- lsIC(x = x, y = y, candidate_models = candidate_models, 
+                    psi = psi, prior = prior, type = weight_type)
+      }
     }
     if (family == "binomial") {
-        if (weight_type == "ARM") {
-            fit <- logitARM(x = x, y = y, candidate_models = candidate_models, 
-                n_train = n_train, no_rep = no_rep, psi = psi, prior = prior)
-        }
-        if (weight_type == "AIC") {
-            fit <- logitIC(x = x, y = y, candidate_models = candidate_models, 
-                psi = psi, prior = prior, type = "AIC")
-        }
-        if (weight_type == "BIC") {
-            fit <- logitIC(x = x, y = y, candidate_models = candidate_models, 
-                psi = psi, prior = prior, type = "BIC")
-        }
+      if (weight_type == "ARM") {
+        fit <- logitARM(x = x, y = y, candidate_models = candidate_models, 
+                        n_train = n_train, no_rep = no_rep, psi = psi, 
+						prior = prior, reduce_bias = reduce_bias)
+      }
+      if (weight_type == "AIC" | weight_type == "BIC") {
+        fit <- logitIC(x = x, y = y, candidate_models = candidate_models, 
+						psi = psi, prior = prior, type = weight_type, 
+						reduce_bias = reduce_bias)
+      }
     }
     weight <- fit$weight
-	# compute VSD etc
-    TMP_matrix <- sweep(candidate_models, MARGIN = 2, model_check, "-")
-    DIFF <- rowSums(abs(TMP_matrix))
-    DIFF_minus <- rowSums(TMP_matrix == -1)
-    DIFF_plus <- rowSums(TMP_matrix == 1)
-    VSD <- weight %*% DIFF  # glmvsd value
-    VSD_minus <- weight %*% DIFF_minus  #false positive
-    VSD_plus <- weight %*% DIFF_plus  #false negative    
-    object <- list(VSD = VSD, VSD_minus = VSD_minus, VSD_plus = VSD_plus, 
-        weight = weight, difference = DIFF, candidate_models_cleaned = candidate_models)
+	  # compute VSD etc
+	  # initialization
+	  no_checkmod <- NROW(model_check)
+	  VSD <- rep(NA, no_checkmod)
+	  VSD_minus <- rep(NA, no_checkmod)
+	  VSD_plus <- rep(NA, no_checkmod)
+	  Fmeasure <- rep(NA, no_checkmod)
+	  Gmeasure <- rep(NA, no_checkmod)
+	  # size of m0
+	  model_check_size <- rowSums(model_check)
+	  # size of mk
+	  candidate_models_size <- rowSums(candidate_models)
+	  # start the loop
+	  for (mindex in seq(no_checkmod)) {
+	    # compare m0 and mk
+	    TMP_matrix <- sweep(candidate_models, MARGIN = 2, model_check[mindex, ], "-")
+	    diff <- rowSums(abs(TMP_matrix))
+	    diff_plus <- rowSums(TMP_matrix == 1)
+	    diff_minus <- rowSums(TMP_matrix == -1)
+	    # compute VSD and VSD plus and minus
+	    VSD[mindex] <- sum(weight*diff)  # glmvsd value
+	    VSD_plus[mindex] <- sum(weight*diff_plus)  # false negative 
+	    VSD_minus[mindex] <- sum(weight*diff_minus)  # false positive
+	    # compute F measure and G measure using precision and recall
+	    Prcision <- (model_check_size[mindex]-diff_minus)/model_check_size[mindex]
+	    Recall <- (model_check_size[mindex]-diff_minus)/candidate_models_size
+	    Fmeasure_tmp <- 2*(Prcision*Recall)/(Prcision+Recall)
+	    Gmeasure_tmp <- sqrt(Prcision*Recall)
+	    Fmeasure[mindex] <- sum(weight*Fmeasure_tmp, na.rm = TRUE)
+	    Gmeasure[mindex] <- sum(weight*Gmeasure_tmp, na.rm = TRUE)
+	  }
+    # output 
+    object <- list(candidate_models_cleaned = candidate_models, VSD = VSD, VSD_minus = VSD_minus, VSD_plus = VSD_plus,  
+	    			Fmeasure = Fmeasure, Gmeasure = Gmeasure,
+                   weight = weight)
     class(object) <- "glmvsd"
     object
 }
